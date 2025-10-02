ac75e0ac57eace313187c5d2d132a0a6a74af43b
nanaplouf <vera78ds@gmail.com>
2025-10-02 14:29:59 +0200

    modification d'un commit

--- src/Controllers/CommitController.php
+++ src/Controllers/CommitController.php
@@ -24,7 +24,7 @@ class CommitController extends AbstractController
             }
             require_once(__DIR__ . "/../Views/addCommit.view.php");
         }else{
-            $this->redirectToRoute('/');
+            $this->redirectToRoute('/', 302);
         }
     }
 
@@ -41,10 +41,41 @@ class CommitController extends AbstractController
             {
                 require_once(__DIR__ . "/../Views/commit.view.php");
             }else{
-                redirectToRoute('/', 302);
+                $this->redirectToRoute('/', 302);
             }
         }else{
-            redirectToRoute('/', 302);
+            $this->redirectToRoute('/', 302);
         }
+    }
+
+    public function editCommit()
+    {
+        if(isset($_GET['id'])){
+            $id = htmlspecialchars($_GET['id']);
+            $commit = new Commit($id, null, null, null, null, null, null, null, null, null, null);
+            $myCommit = $commit->getCommitById();
+            
+            if($myCommit && ($_SESSION['user']['id_user'] === $myCommit->getUserId())){
+
+                if(isset($_POST['editCommit'])){
+                    $text = htmlspecialchars($_POST['commit']);
+                    $this->totalCheck('commit', $text);
+
+                    if(empty($this->arrayError)){
+                        $today = date("Y-m-d");
+                        $updateCommit = new Commit($id, $text, null, $today, null, null, null, null, null, null);
+                        $updateCommit->editCommit();
+                        $this->redirectToRoute('/commit?id='.$id , 200);
+                    }
+                }
+
+                require_once(__DIR__ . "/../Views/editCommit.view.php");
+            }else{
+                $this->redirectToRoute('/', 302);
+            }
+        }else{
+            $this->redirectToRoute('/', 302);
+        }
+        
     }
 }
